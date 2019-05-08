9e8e37f668e5786a591187d1df1dbc390e14c919
jogoo8254 <kev_inc88@hotmail.com>
2019-05-08 14:42:17 -0700

    set props

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import { Server } from 'net';
 
 // A React component is a function that returns a React element.
-// React elements are created with the `React.createElement()` method
-// or JSX tags
+// React elements are created with the `React.createElement()` method or JSX tags
 
 // Your React component's names must be in PascalCase. Those whose names do not
 // begin with upper case letter will interpret it as plain HTML tag
@@ -23,19 +22,19 @@ function AnswerDetails(){
         )
 }
 
-function QuestionDetails(){
+function QuestionDetails(props){
     return(
         <section>
             <div>
                 <h2>
-                    What is your favourite color?
+                    {props.title}
                 </h2>
                 <p>
-                    Red, green, blue, magenta, etc. <br/>
-                    By Bridge Troll
+                    {props.body} <br/>
+                    By {props.author && props.author.full_name}
                 </p>
                 <p>
-                    <small>Seen 10 time(s)</small> - <small>Created 10 days ago</small>
+                    <small>Seen {props.view_count} time(s)</small> - <small>Created {props.created_at} days ago</small>
                 </p>
             </div>
         </section>
@@ -47,7 +46,12 @@ function QuestionDetails(){
 function QuestionShowPage(){
     return (
         <main>
-            <QuestionDetails/>
+            <QuestionDetails title="What's your favourite colour?"
+            body="Red,, green, blue, seaform green, turquoise, etc."
+            author={{full_name: "Bridge Troll"}}
+            view_count = {100}
+            created_at={new Date().toLocaleString()}
+            />
             <AnswerDetails/>
         </main>
     )
@@ -62,7 +66,6 @@ function App(){
         </div>
     )
 }
-
 
 ReactDOM.render(<App />,document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
