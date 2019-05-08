91fe7e4b84650d3d385523cbc40e89b0575bb809
jogoo8254 <kev_inc88@hotmail.com>
2019-05-08 14:19:40 -0700

    Add solution for answer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello, World</h1>, document.getElementById('root'));
+// A React component is a function that returns a React element.
+// React elements are created with the `React.createElement()` method
+// or JSX tags
 
+// Your React component's names must be in PascalCase. Those whose names do not
+// begin with upper case letter will interpret it as plain HTML tag
+
+function AnswerDetails(){
+    return(
+        <div>
+            <p>
+                Red. <br/>
+                By Ulises Wisozk <br/>
+                <small>Answered 2 days ago</small>
+            </p>
+        </div>
+        )
+}
+
+function QuestionDetails(){
+    return(
+        <section>
+            <div>
+                <h2>
+                    What is your favourite color?
+                </h2>
+                <p>
+                    Red, green, blue, magenta, etc. <br/>
+                    By Bridge Troll
+                </p>
+                <p>
+                    <small>Seen 10 time(s)</small> - <small>Created 10 days ago</small>
+                </p>
+            </div>
+        </section>
+    )
+}
+
+// ReactDOM.render(<QuestionDetails />, document.getElementById('root'));
+ReactDOM.render(<AnswerDetails />,document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
