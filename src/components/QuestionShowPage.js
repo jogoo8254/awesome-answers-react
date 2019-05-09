909e015cf0de1b49a1d9999e94ed315b50fba57e
jogoo8254 <kev_inc88@hotmail.com>
2019-05-09 13:50:42 -0700

    turn QuestionShowPage component into class

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import {QuestionDetails} from "./QuestionDetails";
 import {AnswerDetails} from "./AnswerDetails";
 import {AnswerList} from "./AnswerList";
@@ -6,17 +6,21 @@ import {AnswerList} from "./AnswerList";
 import questionData from "../questionData"
 // To structure our app, we'll create components that simulate the pages of web application.
 // These are meant to replace various pages rendered by the routes of our rails Server.
-export function QuestionShowPage(){
-    return (
-        <main className="Page">
-            <QuestionDetails title="What's your favourite colour?"
-            body="Red,, green, blue, seaform green, turquoise, etc."
-            author={{full_name: "Bridge Troll"}}
-            view_count = {100}
-            created_at={new Date().toLocaleString()}
-            />
-            <h2>Answers</h2>
-            <AnswerList answers={questionData.answers}/>
-        </main>
-    )
+
+export class QuestionShowPage extends Component {
+    render(){
+        return (
+            <main className="Page">
+                <QuestionDetails
+                    title="What's your favourite colour?"
+                    body="Red,, green, blue, seaform green, turquoise, etc."
+                    author={{full_name: "Bridge Troll"}}
+                    view_count = {100}
+                    created_at={new Date().toLocaleString()}
+                />
+                <h2>Answers</h2>
+                <AnswerList answers={questionData.answers}/>
+            </main>
+        )
+    }
 }
