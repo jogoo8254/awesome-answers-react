8379e798a60741e410759d47715ee150c222161b
jogoo8254 <kev_inc88@hotmail.com>
2019-05-09 15:02:47 -0700

    Delete questions from QuestionsIndexPage

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -6,6 +6,19 @@ export class QuestionIndexPage extends Component {
         this.state={
             questions: data
         }
+
+        window.questions = data;
+    }
+
+    deleteQuestion(id){
+        console.log(id)
+        // to change state in a React component, you must use the `setstate()` method on `this`.
+        // It takes an object that gets merged in the current state at React's convenience.
+        // The properties in `setState()` replace the same name properties in the current state.
+        // This happens asynchronously and will eventually trigger and update to DOM if there's any change.
+        this.setState({
+            questions: this.state.questions.filter(q => q.id !== id)
+        })
     }
     render() {
       return (
@@ -19,7 +32,10 @@ export class QuestionIndexPage extends Component {
           >
             {this.state.questions.map(question => (
               <li key={question.id}>
-                <a href="">{question.title}</a>
+                <a href="">{question.title}</a> <br />
+                <button onClick={()=> this.deleteQuestion(question.id)
+                    // event=> console.log(event.target)
+                }>Delete</button>
               </li>
             ))}
           </ul>
