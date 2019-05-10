48b197df89274501bb2f12a274c1014858755e21
jogoo8254 <kev_inc88@hotmail.com>
2019-05-10 15:08:29 -0700

    install react-router and route our page components

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import {QuestionShowPage} from "./QuestionShowPage";
 import {QuestionIndexPage} from "./QuestionIndexPage";
 import { Clock } from "./Clock";
+import { WelcomePage } from "./Welcome";
 // In React application, we create a componenet that acts as the "root" or the entry point to
 // all of our other components.
 // This is the one that should be rendered `ReactDOM.render()` 
 function App(){
     return (
+        <BrowserRouter>
         <div>
             <Clock/>
-            <QuestionIndexPage />
-            <QuestionShowPage />
+            <Route exact path="/" component={WelcomePage} />
+            {/* /questions/199 */}
+            <Route path="/questions/:id" component={QuestionShowPage} />
+            <Route exact path="/questions" component={QuestionIndexPage} />
         </div>
+        </BrowserRouter>
     )
 }
 
---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function WelcomePage (props){
+    return(
+        <main className="Page">
+            <h1>Awesome Answers</h1>
+            <p>Synegizing your questions with fantastic answers!</p>
+        </main>
+    )
+}
