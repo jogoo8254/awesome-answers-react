01e990646d91d59bad1404825bca3f1a0df516e9
jogoo8254 <kev_inc88@hotmail.com>
2019-05-14 13:36:39 -0700

    add missing import: Redirect

--- src/components/AuthRoute.js
+++ src/components/AuthRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 
 export const AuthRoute = (props)=>{
     const {isAuthenticated, component: Component, ...routeProps}=props;
